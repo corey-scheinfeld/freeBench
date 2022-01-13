5ef80c7d0ebe71e6f8258567d2cd48354d17a918
Corey <cscheinfeld@middlebury.edu>
2022-01-13 13:08:58 -0500

    fix print statements + directory paths, run_bench in main

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 import signal
 import platform
 import sys
-from subprocess import check_call, check_output, call
+from subprocess import check_call, check_output, call 
 from os.path import join
 from datetime import datetime
 
@@ -17,7 +17,7 @@ KERN_INDEX_FILE  = '/iteration'
 LOCAL_GRUB_FILE  = '/grub'
 KERN_LIST_FILE   = '/kern_list' 
 RESULT_DIR       = '/RESULT_DIR/'
-TEST_DIR         = '/TEST_DIR/'
+TEST_DIR         = 'TEST_DIR/'
 TEST_NAME        = 'OS_Eval'
 
 
@@ -47,7 +47,7 @@ def generate_grub_file(f, target_kern):
     if not os.path.exists(f):
         raise ValueError("File %s does not exist." % f)
 
-    kern_image_name = 'vmlinuz-%s' % target_kern
+    kern_image_name ='kernel' 
     if not os.path.exists(os.path.join('/', 'boot', kern_image_name)):
         raise ValueError('Kernel image %s does not exist' % kern_image_name)
 
@@ -123,13 +123,14 @@ def run_bench():
 
 
 if __name__ == '__main__':
-
+    run_bench()
+"""
     # Setting up working directory and sanity checks.
     if not os.geteuid() == 0:
         raise Exception('This script should be run with "sudo".')
 
     try:
-        WORKING_DIR = os.environ['LEBENCH_DIR']
+        WORKING_DIR = "home/corey/LEBench/" 
     except:
         raise ValueError('$LEBENCH_DIR is not set. Example: "/home/username/LEBench/".')
 
@@ -137,10 +138,10 @@ if __name__ == '__main__':
         raise ValueError('$LEBENCH_DIR should point to the directory containing LEBench. Example: "/home/username/LEBench/".')
 
     KERN_INDEX_FILE = WORKING_DIR + KERN_INDEX_FILE
-    LOCAL_GRUB_FILE = WORKING_DIR + LOCAL_GRUB_FILE
-    KERN_LIST_FILE  = WORKING_DIR + KERN_LIST_FILE
-    RESULT_DIR      = WORKING_DIR + RESULT_DIR
-    TEST_DIR        = WORKING_DIR + TEST_DIR
+    LOCAL_GRUB_FILE = '/' + WORKING_DIR + LOCAL_GRUB_FILE
+    KERN_LIST_FILE  = "/home/corey/LEBench/kern_list" 
+    RESULT_DIR      = '/' + WORKING_DIR + RESULT_DIR
+    TEST_DIR        = '/' + WORKING_DIR + TEST_DIR
 
     if not os.path.exists(KERN_LIST_FILE):
         raise IOError('Cannot open "kern_list" file. If it\'s not present, '
@@ -156,22 +157,22 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         kern_version = sys.argv[1]
         print("[INFO] Configuring to boot into " + kern_version + ".")
-        generate_grub_file(WORKING_DIR + 'template/grub', kern_version)
+        generate_grub_file('/home/corey/LEBench/template/grub', kern_version)
         install_grub_file()
         sys.exit(0)
 
 
     # For running LEBench on a list of specified kernel versions.
     if not os.path.exists(KERN_INDEX_FILE):
-        with open(KERN_INDEX_FILE, 'w') as f:
+        with open('/home/corey/LEBench//iteration', 'w') as f:
             f.write("-1\n")
 
-    with open(KERN_INDEX_FILE, 'r') as f:
+    with open('/home/corey/LEBench//iteration', 'r') as f:
         kern_idx = int(f.read())
     next_kern_idx = kern_idx + 1
     if DEBUG: print('[DEBUG] Running at kernel index: ' + str(kern_idx))
     
-    with open(KERN_INDEX_FILE, 'w') as fp:
+    with open('/home/corey/LEBench//iteration', 'w') as fp:
         fp.write(str(next_kern_idx).strip())
 
     if DEBUG: print('[DEBUG] Done writing kernel index %d for the next iteration' % next_kern_idx + '.')
@@ -184,9 +185,9 @@ if __name__ == '__main__':
         run_bench()
 
     if DEBUG: print('[DEBUG] Preparing to modify grub.')
-    if generate_grub_file(WORKING_DIR + 'template/grub', get_kern_list(next_kern_idx)):
+    if generate_grub_file('/'+WORKING_DIR + 'template/grub', get_kern_list(next_kern_idx)):
         install_grub_file()
         if DEBUG: print('[DEBUG] Done configuring grub for the next kernel.')
         restart()
-
+"""
 
