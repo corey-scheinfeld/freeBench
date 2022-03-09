70e582e0efdd4dedcaabf186f151cb4cc4969b13
corey <corey@FreeBSD-10.4>
2022-03-09 18:43:48 -0500

    update mmap pointer

--- src/munmap_test.c
+++ src/munmap_test.c
@@ -5,7 +5,7 @@ void munmap_test(struct timespec *diffTime) {
 
 	int fd =open("test_file.txt", O_RDWR);
 	if(fd < 0) printf("invalid fd%d\n", fd);
-	void *addr = (void *)(intptr_t)__syscall(SYS_mmap, NULL, file_size, PROT_WRITE, MAP_PRIVATE, fd, 0);
+	void *addr = (void *)(uintptr_t)__syscall(SYS_mmap, NULL, file_size, PROT_WRITE, MAP_PRIVATE, fd, 0);
 	for (int i = 0; i < file_size; i++) {
 		((char *)addr)[i] = 'b';
 	}
