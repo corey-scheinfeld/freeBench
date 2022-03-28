957c26761fd929dae6687208003bbeee3fa48e0e
corey-scheinfeld <cscheinfeld@middlebury.edu>
2022-03-27 20:52:04 -0400

    fork parent/child data split

--- stat_analysis/analyze.py
+++ stat_analysis/analyze.py
@@ -4,7 +4,8 @@ import statistics as stat
 import os
 
 INPERCISION = 0.001
-K = 100 
+K = 100
+
 
 def kBest(timeArray, size):
     timeArray.sort()
@@ -20,29 +21,32 @@ def kBest(timeArray, size):
             j = 0
         else:
             diff = float(curr - prev)
-            ratioDiff = float( diff  / float(prev))
+            ratioDiff = float(diff / float(prev))
             if(ratioDiff > INPERCISION):
                 j = 1
                 k_closest = [None] * K
                 k_closest[0] = curr
             else:
                 k_closest[j] = curr
-                j+=1
+                j += 1
 
-        if(j == K): break
+        if(j == K):
+            break
         prev = curr
 
     result = k_closest[0]
     return result
 
+
 def analyze(raw_data, output):
     raw_file = open(raw_data, mode='r')
-    out_file = open(output, mode = 'a')
+    out_file = open(output, mode='a')
 
     fieldnames = ["test", "avg", "min", "max", "kbest", "variance", "stddev"]
 
     raw_csv = list(csv.reader(raw_file, delimiter=','))
-    out_csv = csv.DictWriter(out_file, fieldnames = fieldnames, extrasaction='ignore')
+    out_csv = csv.DictWriter(
+        out_file, fieldnames=fieldnames, extrasaction='ignore')
 
     file = raw_data.split("/")
     test = file[len(file)-1].replace("_data.csv", "")
@@ -53,29 +57,25 @@ def analyze(raw_data, output):
     data = []
     data2 = []
     for i in range(2, len(raw_csv)-1):
-        line = raw_csv[i]
-         
-        if(two_line_test):
+        if(not two_line_test or i % 2 == 0):
+            line = raw_csv[i]
             data.append(float(line[2]))
-            data2.append(float(raw_csv[i+1][2]))
-            i+=1
         else:
-            data.append(float(line[2]))
+            data2.append(float(raw_csv[i][2]))
 
     out_csv.writeheader()
 
-    out_csv.writerow({'test': (("parent "+str(test)) if two_line_test else str(test)), 'avg':'{0:.09f}'.format(stat.mean(data)), 'min':'{0:.09f}'.format(min(data)), \
-        'max':'{0:.09f}'.format(max(data)), 'kbest': '{0:.09f}'.format(kBest(data, len(data))), 'variance':'{0:.09f}'.format(stat.variance(data)), 'stddev': '{0:.09f}'.format(stat.stdev(data))})
+    out_csv.writerow({'test': (("parent "+str(test)) if two_line_test else str(test)), 'avg': '{0:.09f}'.format(stat.mean(data)), 'min': '{0:.09f}'.format(min(data)),
+                      'max': '{0:.09f}'.format(max(data)), 'kbest': '{0:.09f}'.format(kBest(data, len(data))), 'variance': '{0:.09f}'.format(stat.variance(data)), 'stddev': '{0:.09f}'.format(stat.stdev(data))})
 
     if(two_line_test):
-        out_csv.writerow({'test':"child "+str(test), 'avg':'{0:.09f}'.format(stat.mean(data2)), 'min':'{0:.09f}'.format(min(data2)), \
-        'max':'{0:.09f}'.format(max(data2)), 'kbest': kBest(data2, len(data2)), 'variance': '{0:.09f}'.format(stat.variance(data2)), 'stddev': '{0:.09f}'.format(stat.stdev(data2))})
-                
-            
-    raw_file.close()
-    out_file.close()      
+        out_csv.writerow({'test': "child "+str(test), 'avg': '{0:.09f}'.format(stat.mean(data2)), 'min': '{0:.09f}'.format(min(data2)),
+                          'max': '{0:.09f}'.format(max(data2)), 'kbest': kBest(data2, len(data2)), 'variance': '{0:.09f}'.format(stat.variance(data2)), 'stddev': '{0:.09f}'.format(stat.stdev(data2))})
 
-            
+    raw_file.close()
+    out_file.close()
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         raise Exception("This program requires a version name and run number")
@@ -91,6 +91,3 @@ if __name__ == '__main__':
             analyze(os.path.join(raw_directory, data_file), out_file)
 
     print("done")
-    
-
-
